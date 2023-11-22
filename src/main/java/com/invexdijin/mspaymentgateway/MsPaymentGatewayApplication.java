f561fc3ea7d79da829726828cb94110bb6a900d5
jmartinez79 <john1992alex@gmail.com>
2023-11-22 10:06:55 -0500

    Initial project.

---
+++ src/main/java/com/invexdijin/mspaymentgateway/MsPaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.invexdijin.mspaymentgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPaymentGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invexdijin/mspaymentgateway/MsPaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invexdijin.mspaymentgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
